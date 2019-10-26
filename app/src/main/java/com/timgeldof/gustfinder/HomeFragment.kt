c0eae435795132e5ed99a03a5103edf25a340139
Tim Geldof <tim.geldof.y3488@student.hogent.be>
2019-10-26 14:59:04 +0200

    Add first navigation trajectory

--- app/src/main/java/com/timgeldof/gustfinder/HomeFragment.kt
+++ app/src/main/java/com/timgeldof/gustfinder/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.timgeldof.gustfinder.databinding.HomeFragmentBinding
 
 /**
@@ -17,6 +18,8 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding: HomeFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.home_fragment,container, false)
+
+        binding.textMyPlaces.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_myPlacesFragment))
         return binding.root
     }
 
