0ccfd6f1a3cafaa79c3e110c951496c0582e5929
Tim Geldof <tim.geldof.y3488@student.hogent.be>
2019-10-26 16:16:24 +0200

    Add navigation to about fragment

--- app/src/main/java/com/timgeldof/gustfinder/HomeFragment.kt
+++ app/src/main/java/com/timgeldof/gustfinder/HomeFragment.kt
@@ -2,13 +2,13 @@ package com.timgeldof.gustfinder
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.timgeldof.gustfinder.databinding.HomeFragmentBinding
 
 /**
@@ -20,7 +20,23 @@ class HomeFragment : Fragment() {
         val binding: HomeFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.home_fragment,container, false)
 
         binding.textMyPlaces.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_myPlacesFragment))
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+
     }
 
 
