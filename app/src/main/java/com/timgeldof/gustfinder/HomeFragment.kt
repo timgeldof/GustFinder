2820ae16cc4cac25a29128d03ff672b16e354e6c
Tim Geldof <tim.geldof.y3488@student.hogent.be>
2019-10-26 13:28:20 +0200

    Add data binding

---
+++ app/src/main/java/com/timgeldof/gustfinder/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.timgeldof.gustfinder
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.timgeldof.gustfinder.databinding.HomeFragmentBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding: HomeFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.home_fragment,container, false)
+        return binding.root
+    }
+
+
+}
