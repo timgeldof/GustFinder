9bf8d8120fea93a31b4fabe8e2bc95a630817bd1
Tim Geldof <tim.geldof.y3488@student.hogent.be>
2019-10-26 15:50:33 +0200

    Add about fragment

---
+++ app/src/main/java/com/timgeldof/gustfinder/AboutFragment.kt
@@ -0,0 +1,27 @@
+package com.timgeldof.gustfinder
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.timgeldof.gustfinder.databinding.AboutFragmentBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<AboutFragmentBinding>(inflater, R.layout.about_fragment, container, false)
+        return binding.root
+    }
+
+
+}
--- app/src/main/java/com/timgeldof/gustfinder/MainActivity.kt
+++ app/src/main/java/com/timgeldof/gustfinder/MainActivity.kt
@@ -2,11 +2,23 @@ package com.timgeldof.gustfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.timgeldof.gustfinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
