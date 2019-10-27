f5d550f546443f68dc863df5a16021c82ff7d3aa
Tim Geldof <tim.geldof.y3488@student.hogent.be>
2019-10-27 02:35:16 +0200

    Add Place entity

--- app/src/main/java/com/timgeldof/gustfinder/database/Place.kt
+++ app/src/main/java/com/timgeldof/gustfinder/database/Place.kt
@@ -1,8 +1,16 @@
 package com.timgeldof.gustfinder.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 
+@Entity(tableName = "places_table")
 data class Place(
+    @PrimaryKey(autoGenerate = true)
     var placeId: Long = 0L,
-    var name: String
+    @ColumnInfo(name = "city")
+    var city: String,
+    @ColumnInfo(name = "country_iso_code")
+    var countryCodeISO: String
 )
