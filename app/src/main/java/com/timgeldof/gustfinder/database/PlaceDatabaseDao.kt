b666bda7b167073a229ff45dc9c538876da28776
Tim Geldof <tim.geldof.y3488@student.hogent.be>
2019-10-27 02:46:24 +0200

    Add database access object

---
+++ app/src/main/java/com/timgeldof/gustfinder/database/PlaceDatabaseDao.kt
@@ -0,0 +1,22 @@
+package com.timgeldof.gustfinder.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface PlaceDatabaseDao {
+
+    @Query("SELECT * from places_table WHERE placeId = :key")
+    fun get(key: Long): Place?
+    @Insert
+    fun insert(night: Place)
+    @Query("SELECT * FROM places_table ORDER BY city DESC")
+    fun getAllPlaces(): LiveData<List<Place>>
+    @Query("DELETE FROM places_table WHERE placeId = :key")
+    fun removePlace(key: Long)
+
+
+}
