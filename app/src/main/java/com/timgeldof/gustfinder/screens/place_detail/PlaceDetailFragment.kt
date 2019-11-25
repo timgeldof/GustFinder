3b347a3adc55f8164267e301dc563c26a6a57ad9
Tim Geldof <Tim.geldof@student.hogent.be>
2019-11-25 20:55:03 +0100

    Add forecasts recyclerview

--- app/src/main/java/com/timgeldof/gustfinder/screens/place_detail/PlaceDetailFragment.kt
+++ app/src/main/java/com/timgeldof/gustfinder/screens/place_detail/PlaceDetailFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -36,6 +37,7 @@ class PlaceDetailFragment : Fragment() {
             adapter.data = it
         })
 
+        Toast.makeText(context, "Swipe right for more", Toast.LENGTH_SHORT)
         return binding.root;
 
 
