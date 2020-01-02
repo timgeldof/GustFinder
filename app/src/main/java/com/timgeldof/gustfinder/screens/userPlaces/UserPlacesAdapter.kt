9b1a4d6c1950dff30ad992288bcbfcce7083ef91
Tim Geldof <Tim.geldof@student.hogent.be>
2020-01-03 00:41:59 +0100

    Fix material textinputlayout visibility

--- app/src/main/java/com/timgeldof/gustfinder/screens/userPlaces/UserPlacesAdapter.kt
+++ app/src/main/java/com/timgeldof/gustfinder/screens/userPlaces/UserPlacesAdapter.kt
@@ -5,7 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.timgeldof.gustfinder.R
 import com.timgeldof.gustfinder.database.Place
 import com.timgeldof.gustfinder.databinding.PlaceItemBinding
 /**
